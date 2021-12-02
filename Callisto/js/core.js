dc1f7c280b37cfdd38122620f8b29edd113849dc
Xtian <cristiancordero@gmail.com>
2021-12-02 10:01:45 -0500

    se agrega el efecto loading y se pasa a llamadas asincronas

--- Callisto/js/core.js
+++ Callisto/js/core.js
@@ -41,35 +41,7 @@ function mostrarInventarios(){
   const urlAccion = 
   "https://script.google.com/macros/s/AKfycbzxBDBc3tYiOH_i6aRdN0S-aaKocbOeA04jJNyE7jTbCZ78AbBSw4ns0b4KZ5y7G6tc5g/exec?action=" 
   + accion;
-  var inventarios = [];
-  inventarios = loadDoc(urlAccion);
-
-  if (inventarios.length===0) {
-    alert('No hay información para mostrar.');
-    return;
-  }
-
-  document.getElementById("menu-inv").innerHTML="";
-
-  inventarios.sort( (a,b) => {
-    if (a.estado > b.estado)
-      return 1;
-    else if (a.estado < b.estado)
-    return -1;
-    return 0;
-  });
-
-inventarios.map(value => {
-  var iDiv = document.createElement('div');
-  iDiv.id = value.codInventario;
-  iDiv.textContent = value.comentario;
-  if (value.estado === "CERRADO")
-  iDiv.className = 'm-item-largo color5';
-  else iDiv.className = 'm-item-largo color8';
-  iDiv.setAttribute('onclick',"mostrarFamilias(id,'" +value.estado + "')");
-  document.getElementById("menu-inv").appendChild(iDiv);
-  irPagina3();
-});
+  loadDocAsincrono(urlAccion,mostrarInventariosCont);
 }
 
 function irPagina3() {
@@ -89,28 +61,7 @@ function mostrarFamilias(codinventario, estado){
   const urlAccion = 
   "https://script.google.com/macros/s/AKfycbzxBDBc3tYiOH_i6aRdN0S-aaKocbOeA04jJNyE7jTbCZ78AbBSw4ns0b4KZ5y7G6tc5g/exec?action=" + 
   accion +  "&codinventario=" + inventarioSelec;
-  var familias = loadDoc(urlAccion);
-  if (familias.length===0) {
-    alert('No hay información para mostrar.');
-    return;
-  }
-
-  familias.push({
-    codinventario: codinventario,
-    familia: "TODAS",
-    estado: "ABIERTO"
-    });
-
-    document.getElementById("menu-familias").innerHTML="";
-  familias.map(value => {
-    var iDiv = document.createElement('div');
-    iDiv.id = value.familia;
-    iDiv.textContent = value.familia;
-    iDiv.className = 'm-item-largo color4';
-    iDiv.setAttribute('onclick',"mostrarProductos(id)");
-    document.getElementById("menu-familias").appendChild(iDiv);
-  });
-  irPagina4();
+  loadDocAsincrono2(urlAccion,mostrarFamiliasCont,codinventario);
 }
 
 function irPagina4() {
@@ -120,38 +71,13 @@ function irPagina4() {
 }
 
 function mostrarProductos(familia){
-familiaSelec = familia;
-const accion = 3;
-const familiaConsulta = familia ==="TODAS"? "" : familia;
-const urlAccion = 
-"https://script.google.com/macros/s/AKfycbzxBDBc3tYiOH_i6aRdN0S-aaKocbOeA04jJNyE7jTbCZ78AbBSw4ns0b4KZ5y7G6tc5g/exec?action=" + 
-accion +  "&codinventario=" + inventarioSelec + "&familia=" + familiaConsulta;
-var productos = loadDoc(urlAccion);
-if (productos.length===0) {
-  alert('No hay información para mostrar.');
-  return;
-}
-
-document.getElementById("menu-productos").innerHTML="";
-
-productos.sort( (a,b) => {
-  if (a.estado > b.estado)
-    return 1;
-  else if (a.estado < b.estado)
-  return -1;
-  return 0;
-});
-
-  productos.map(value => {
-    var iDiv = document.createElement('div');
-    iDiv.id = value.codFadrisac;
-    iDiv.textContent = value.codBando;
-    iDiv.className = value.estado === 'ABIERTO'? 'm-item-largo color3':'m-item-largo color8'
-    iDiv.setAttribute('onclick',"irPagina5_1(id,textContent," + value.stockConteo + ")");
-    document.getElementById("menu-productos").appendChild(iDiv);
-  });
-  irPagina5();
-
+  familiaSelec = familia;
+  const accion = 3;
+  const familiaConsulta = familia ==="TODAS"? "" : familia;
+  const urlAccion = 
+  "https://script.google.com/macros/s/AKfycbzxBDBc3tYiOH_i6aRdN0S-aaKocbOeA04jJNyE7jTbCZ78AbBSw4ns0b4KZ5y7G6tc5g/exec?action=" + 
+  accion +  "&codinventario=" + inventarioSelec + "&familia=" + familiaConsulta;
+  loadDocAsincrono(urlAccion,mostrarProductosCont);
 }
 
 function irPagina5() {
@@ -174,18 +100,59 @@ function cerrarPagina5_1() {
 }
 
 
-function loadDoc(dirUrl) {
+function loadDocSincrono(dirUrl) {
   var xhttp = new XMLHttpRequest();
   var temp = "";
   xhttp.onreadystatechange = function() {
       if(this.readyState == 1) {
+        mostrarLoading();
       }
       if (this.readyState == 4 && this.status == 200) {
+        ocultarLoading();
         temp = JSON.parse(this.responseText);
           return JSON.parse(this.responseText);
      }
   };
   xhttp.open("GET", dirUrl, false);
+  xhttp.send();
+  return temp;
+  
+}
+
+function loadDocAsincrono(dirUrl,procesarRpta) {
+  var xhttp = new XMLHttpRequest();
+  var temp = "";
+  xhttp.onreadystatechange = function() {
+      if(this.readyState == 1) {
+        mostrarLoading();
+      }
+      if (this.readyState == 4 && this.status == 200) {
+        ocultarLoading();
+        temp = JSON.parse(this.responseText);
+        procesarRpta(temp);
+          return JSON.parse(this.responseText);
+     }
+  };
+  xhttp.open("GET", dirUrl, true);
+  xhttp.send();
+  return temp;
+  
+}
+function loadDocAsincrono2(dirUrl,procesarRpta,variable1) {
+  var xhttp = new XMLHttpRequest();
+  var temp = "";
+  xhttp.onreadystatechange = function() {
+      if(this.readyState == 1) {
+        mostrarLoading();
+      }
+      if (this.readyState == 4 && this.status == 200) {
+        ocultarLoading();
+        temp = JSON.parse(this.responseText);
+        procesarRpta(temp,variable1);
+          return JSON.parse(this.responseText);
+     }
+  };
+  xhttp.open("GET", dirUrl, true);
   xhttp.send();
   return temp;
   
@@ -231,15 +198,7 @@ function enviarConteo(tipo){
   "https://script.google.com/macros/s/AKfycbzxBDBc3tYiOH_i6aRdN0S-aaKocbOeA04jJNyE7jTbCZ78AbBSw4ns0b4KZ5y7G6tc5g/exec?action=" + 
   accion +  "&codinventario=" + inventarioSelec + "&codfadrisac=" + codfadrisac + "&conteo=" + stockConteo + 
   "&usuario=" + usuario;
-  var guardado = loadDoc(urlAccion);
-  if (guardado)
-  alert("Grabado exitoso.");
-  else alert("No se pudo grabar.");
- 
-  document.getElementById("nuevaCantidad").value="";
-  cerrarPagina5_1();
-  //document.getElementById(codfadrisac).className = 'm-item-largo color8';
-
+  loadDocAsincrono(urlAccion,enviarConteoCont);
 }
 
 function buscarProducto(){
@@ -248,14 +207,7 @@ function buscarProducto(){
   const urlAccion = 
   "https://script.google.com/macros/s/AKfycbzxBDBc3tYiOH_i6aRdN0S-aaKocbOeA04jJNyE7jTbCZ78AbBSw4ns0b4KZ5y7G6tc5g/exec?action=" + 
   accion +  "&codinventario=" + inventarioSelec + "&patron=" + patron
-  var resultadoBusq = loadDoc(urlAccion);
-  if (resultadoBusq.length===0) {
-    alert('No se encontró el producto.');
-    return;
-  }
-  console.log(resultadoBusq); 
-  
-  mostrarResultadoBusqueda(resultadoBusq);
+  loadDocAsincrono(urlAccion,buscarProductoCont);
 }
 
 function mostrarResultadoBusqueda(resultado){
@@ -288,13 +240,123 @@ function cerrarPagina6() {
 }
 
 function mostrarLoading(){
-  //document.getElementById("loading").style.display = "inline";
-  //document.getElementById('loading').visible = true;
-  //document.getElementById("contenedor").style.visibility='hidden';
+  
+  /*$("body").loading({
+    message: "Cargando",
+    theme: "dark"
+  });
+  $("body").loading("start");*/
+
+  document.getElementById("loading").style.display = "inline";
 }
 
 function ocultarLoading(){
-  //document.getElementById("loading").style.display = "none";
-  //document.getElementById("loading").visible = false;
-  //document.getElementById("contenedor").style.visibility='visible';
+  /*$("body").loading("stop");*/
+  document.getElementById("loading").style.display = "none";
+}
+
+function mostrarInventariosCont(rpta){
+    var inventarios = [];
+    inventarios = rpta;
+      if (inventarios.length===0) {
+        alert('No hay información para mostrar.');
+        return;
+      }
+
+      document.getElementById("menu-inv").innerHTML="";
+
+      inventarios.sort( (a,b) => {
+        if (a.estado > b.estado)
+          return 1;
+        else if (a.estado < b.estado)
+        return -1;
+        return 0;
+      });
+
+    inventarios.map(value => {
+      var iDiv = document.createElement('div');
+      iDiv.id = value.codInventario;
+      iDiv.textContent = value.comentario;
+      if (value.estado === "CERRADO")
+      iDiv.className = 'm-item-largo color5';
+      else iDiv.className = 'm-item-largo color8';
+      iDiv.setAttribute('onclick',"mostrarFamilias(id,'" +value.estado + "')");
+      document.getElementById("menu-inv").appendChild(iDiv);
+      irPagina3();
+    });
+}
+
+function mostrarProductosCont(rpta){
+    var productos = rpta;
+    if (productos.length===0) {
+      alert('No hay información para mostrar.');
+      return;
+    }
+    
+    document.getElementById("menu-productos").innerHTML="";
+    
+    productos.sort( (a,b) => {
+      if (a.estado > b.estado)
+        return 1;
+      else if (a.estado < b.estado)
+      return -1;
+      return 0;
+    });
+    
+      productos.map(value => {
+        var iDiv = document.createElement('div');
+        iDiv.id = value.codFadrisac;
+        iDiv.textContent = value.codBando;
+        iDiv.className = value.estado === 'ABIERTO'? 'm-item-largo color3':'m-item-largo color8'
+        iDiv.setAttribute('onclick',"irPagina5_1(id,textContent," + value.stockConteo + ")");
+        document.getElementById("menu-productos").appendChild(iDiv);
+      });
+      irPagina5();
+}
+
+function enviarConteoCont(rpta){
+  var guardado = rpta;
+  if (guardado)
+  alert("Grabado exitoso.");
+  else alert("No se pudo grabar.");
+ 
+  document.getElementById("nuevaCantidad").value="";
+  cerrarPagina5_1();
+  //document.getElementById(codfadrisac).className = 'm-item-largo color8';
+}
+
+function buscarProductoCont(rpta){
+  var resultadoBusq = rpta;
+  if (resultadoBusq.length===0) {
+    alert('No se encontró el producto.');
+    return;
+  }
+  console.log(resultadoBusq); 
+  
+  mostrarResultadoBusqueda(resultadoBusq);
+}
+
+function mostrarFamiliasCont(rpta,codinventario){
+  var familias = rpta;
+  if (familias.length===0) {
+    alert('No hay información para mostrar.');
+    return;
+  }
+
+  familias.push({
+    codinventario: codinventario,
+    familia: "TODAS",
+    estado: "ABIERTO"
+    });
+
+    document.getElementById("menu-familias").innerHTML="";
+  familias.map(value => {
+    var iDiv = document.createElement('div');
+    iDiv.id = value.familia;
+    iDiv.textContent = value.familia;
+    iDiv.className = 'm-item-largo color4';
+    iDiv.setAttribute('onclick',"mostrarProductos(id)");
+    document.getElementById("menu-familias").appendChild(iDiv);
+  });
+  irPagina4();
 }
